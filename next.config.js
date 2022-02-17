49c11b97047bb73ba780063a8206faebbb428dc3
viafoura-demo <99916751+viafoura-demo@users.noreply.github.com>
2022-02-17 17:43:10 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ module.exports = {
   reactStrictMode: true,
   env: {
     GRAPHQL_API_TOKEN: process.env.GRAPHQL_API_TOKEN,
-    vfDomain: "demo.viafoura.com",
-    vfSiteId: "233586191803414",
-    vfSiteUUID: "00000000-0000-4000-8000-d47205fca416",
+    vfDomain: "qa-demo-site.vercel.app",
+    vfSiteId: "179672139435639",
+    vfSiteUUID: "00000000-0000-4000-8000-a3692e0c0e77",
   },
   images: {
     domains: ["www.datocms-assets.com"],
