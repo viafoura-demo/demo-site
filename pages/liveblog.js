04315f8fabc407fef0a8027806bd7743c2120d32
viafoura-demo <99916751+viafoura-demo@users.noreply.github.com>
2023-10-18 15:24:10 -0500

    Create liveblog

---
+++ pages/liveblog.js
@@ -0,0 +1,11 @@
+import Container from "@/components/container";
+
+export default function LiveBlog() {
+  return (
+    <Container>
+      <div className="viafoura">
+        <vf-live-blog vf-container-id="1234567"></vf-live-blog>
+      </div>
+    </Container>
+  );
+}
